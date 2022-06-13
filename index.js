8d90dddebbd6d9e4cf86808e76647af8be8e63fe
Elijah <bundielly@gmail.com>
2022-06-13 06:50:03 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 function addingEventListener() {
+    const input = document.getElementById('input')
+    function clickAlert() {
+        alert('Hee! Hee! that tickles!')
+    }
+    input.addEventListener('click', clickAlert)
 }
+
+addingEventListener()
+
+
+
+
+
+
+
+
